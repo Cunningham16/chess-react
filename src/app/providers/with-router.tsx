ac7c6ec34606d90dca21036838f946f97e971409
Ivan <3785256@gmail.com>
2023-03-16 12:26:59 +0300

    adding loader

--- src/app/providers/with-router.tsx
+++ src/app/providers/with-router.tsx
@@ -1,9 +1,4 @@
-import { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 export const withRouter = (component: () => React.ReactNode) => () =>
-  (
-    <BrowserRouter>
-      <Suspense>{component()}</Suspense>
-    </BrowserRouter>
-  );
+  <BrowserRouter>{component()}</BrowserRouter>;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { SideBar } from "widgets/Sidebar";
 
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import { ThreeDots } from "react-loader-spinner";
 
 const PlayWithFriend = lazy(() => import("./PlayWithFriend"));
 const MainPage = lazy(() => import("./MainPage"));
@@ -12,12 +13,33 @@ const Routing = () => {
   return (
     <div>
       <SideBar />
-      <Routes>
-        <Route path="/" element={<MainPage />}></Route>
-        <Route path="/playfriend" element={<PlayWithFriend />}></Route>
-        <Route path="/playai" element={<PlayWithAI />}></Route>
-        <Route path="/settings" element={<Settings />}></Route>
-      </Routes>
+      <Suspense
+        fallback={
+          <ThreeDots
+            height="80"
+            width="80"
+            radius="9"
+            color="#ca8745"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{
+              display: "flex",
+              width: "100%",
+              height: "100vh",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            wrapperClass=""
+            visible={true}
+          />
+        }
+      >
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/playfriend" element={<PlayWithFriend />}></Route>
+          <Route path="/playai" element={<PlayWithAI />}></Route>
+          <Route path="/settings" element={<Settings />}></Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
